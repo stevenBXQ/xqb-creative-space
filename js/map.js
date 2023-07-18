fe0bea4702c058b282f01d09c1e449bb55a5b59d
Steven Bao <whichsteven297@gmail.com>
2023-07-18 16:24:27 -0700

    kinda finished

--- js/map.js
+++ js/map.js
@@ -3,40 +3,73 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/whichsteven/clk3dched004x01pqg2uv87y0", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  center: [-122.30728694154617, 47.655552182760125], // starting position [lng, lat]
+  zoom: 1, // starting zoom
+  projection: 'globe'
+
 });
 
-// const job_torc = document.getElementById("test");
+map.on('style.load', () => {
+  map.setFog({
+      color: 'rgb(186, 210, 235)', // Lower atmosphere
+      'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+      'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+      'space-color': 'rgb(11, 11, 25)', // Background color
+      'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
+  });
+});
 
-// // This handler will be executed only once when the cursor
-// // moves over the unordered list
-// test.addEventListener(
-//   "mouseenter",
-//   (event) => {
-//     // highlight the mouseenter target
-//     event.target.style.color = "purple";
+const job_torc = document.getElementById("job_torc");
 
-//     // reset the color after a short delay
-//     setTimeout(() => {
-//       event.target.style.color = "";
-//     }, 500);
-//   },
-//   false,
-// );
+job_torc.addEventListener("mouseenter", (event) => {
+  map.flyTo({
+    center: [-80.3923385938034, 37.18948135807281],
+    zoom: 5,
+    // minZoom: 5,
+    speed: 1.0,
+  });
+});
 
-function flyToBlacksburg() {
-    map.flyTo({center: [-80.3923385938034, 37.18948135807281], zoom: 11, minZoom: 5});
-}
+const job_nokia = document.getElementById("job_nokia");
 
-function flyToDallas() {
-    map.flyTo({center: [-96.98406522152219, 32.93687941157875], zoom: 10, minZoom: 5});
-}
+job_nokia.addEventListener("mouseenter", (event) => {
+  map.flyTo({
+    center: [-96.98406522152219, 32.93687941157875],
+    zoom: 5,
+    // minZoom: 5,
+    speed: 1.0,
+  });
+});
 
-function flyToSeattle() {
-    map.flyTo({center: [-122.30728694154617, 47.655552182760125], zoom: 10, minZoom: 5});
-}
+const job_ta = document.getElementById("job_ta");
 
-function flyToPNW() {
-    map.flyTo({center: [-122.30728694154617, 47.655552182760125], zoom: 10, minZoom: 5});
-}
+job_ta.addEventListener("mouseenter", (event) => {
+  map.flyTo({
+    center: [-122.30728694154617, 47.655552182760125],
+    zoom: 5,
+    // minZoom: 5,
+    speed: 1.0,
+  });
+});
+
+const job_ra = document.getElementById("job_ra");
+
+job_ra.addEventListener("mouseenter", (event) => {
+  map.flyTo({
+    center: [-122.30728694154616, 47.655552182760125],
+    zoom: 5,
+    // minZoom: 5,
+    speed: 1.0,
+  });
+});
+
+const job_rf = document.getElementById("job_rf");
+
+job_ta.addEventListener("mouseenter", (event) => {
+  map.flyTo({
+    center: [-122.30728694154617, 47.655552182760124],
+    zoom: 5,
+    // minZoom: 5,
+    speed: 1.0,
+  });
+});
